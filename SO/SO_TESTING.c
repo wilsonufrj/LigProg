1fa428f6371575cb4f43eb97e6b6ee4ef09f3f5d
Wilson <waramos97@gmail.com>
2021-03-31 20:46:34 -0300

    inclusao do app de teste SO

---
+++ SO/SO_TESTING.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[]){
+    printf("Hello World (pid %d)\n",(int)getpid());
+    int rc = 0;
+    if(rc<0){
+            fprintf(stderr,"fork failed\n");
+            exit(1);
+    }else{
+        if(rc ==0){
+            printf("Hello,i'm a child (pid %d)\n",(int)getpid);
+        }else{
+            printf("Hello, i'm a parent of %d (pid:%d)\n",rc,(int)getpid());
+        }
+    }
+    return 0;
+}
