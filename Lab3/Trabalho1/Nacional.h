cbd3a9d62aa117004538a654691e41730332d884
Wilson <waramos97@gmail.com>
2021-04-12 10:59:38 -0300

    Inicio do Trabalho 1, bug com a entrada

---
+++ Lab3/Trabalho1/Nacional.h
@@ -0,0 +1,19 @@
+#include"Estadual.h"
+#include<vector>
+
+using namespace std;
+
+class Nacional{
+
+    private:
+    vector<int> estados;
+
+    public:
+    void mostrarEvolucaoObitoNacionalEstadual();
+    void mostrarEstatisticasEstados();
+    void mostratEstatisticaBrasil();
+    void estadosLimites();
+    void mostrarNumeroObitosNacionalEstadual();
+};
+
+
---
+++ Lab3/Trabalho1/index.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include"Nacional.h"
+
+using namespace std;
+
+int main(){
+    cout<<"\tBem-vindo a Estatísticas COVID-2021\n"<<endl;
+    cout<<"\t                        MENU                           "<<endl;
+    cout<<"\t|------------------------------------------------------|"<<endl;
+    cout<<"\t| PRESSIONA UMA DAS OPCOES ABAIXO                      |"<<endl;
+    cout<<"\t| 0 - Exibir a media movel no Brasil e nos estados     |"<<endl;
+    cout<<"\t| 1 - Exibir situacao nos estados                      |"<<endl;
+    cout<<"\t| 2 - Exibir situacao no Brasil                        |"<<endl;
+    cout<<"\t| 3 - Exibir estados com maior alta e menor baixa      |"<<endl;
+    cout<<"\t| 4 - Exibir numero de obitos                          |"<<endl;
+    cout<<"\t| 5 - SAIR                                             |"<<endl;
+    cout<<"\t|------------------------------------------------------|"<<endl;
+    int opcaoMenu;
+    //Nacional Brasil;
+
+    while(1){
+        //Pesquisar outro metodo de entrada
+        cin>>opcaoMenu;
+        switch(opcaoMenu){
+            case 0:
+                cout<<"mostrarEvolucaoObitoNacionalEstadual"<<endl;
+                //Brasil.mostrarEvolucaoObitoNacionalEstadual();
+                break;
+            case 1:
+                cout<<"mostrarEstatisticasEstados()"<<endl;
+                //Brasil.mostrarEstatisticasEstados();
+                break;
+            case 2:
+                cout<<"mostratEstatisticaBrasil()"<<endl;
+                //Brasil.mostratEstatisticaBrasil();
+                break;
+            case 3:
+                cout<<"mostrarEstadosLimites"<<endl;
+                //Brasil.estadosLimites();
+                break;
+            case 4:
+                cout<<"mostrarNumeroObitosNacionalEstadual"<<endl;
+                //Brasil.mostrarNumeroObitosNacionalEstadual();
+                break;
+            case 5:
+                break;
+            default:
+                cout<<"Opcao Invalida"<<endl;
+        }
+
+        if(opcaoMenu==5)
+            break;
+        
+    }
+    return 0;
+}
