a8d88fd029116e7064aaf262f8d80a0581ed6cf7
Wilson <waramos97@gmail.com>
2021-06-09 20:36:19 -0300

    Inicio do projeto final

---
+++ TrabalhoFinal/MySQL_Connection.h
@@ -0,0 +1,12 @@
+#ifndef MYSQL_CONNECTION_H
+#define MYSQL_CONNECTION_H
+
+
+class MySQL_Connection{
+    public:
+
+    private:
+    const char *server, *user, *password, *database;
+};
+
+#endif
---
+++ TrabalhoFinal/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"MySQL_Connection.h"
+
+
+int main(){
+
+    return 0;
+}
